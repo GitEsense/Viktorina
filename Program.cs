83fdeb613b86d1c96c822d3dedab5f96d40073b6
Kld_Esense <klg-hunter@mail.ru>
2022-03-13 18:58:30 +0200

    Оптимизация кода

--- Program.cs
+++ Program.cs
@@ -1,28 +1,29 @@
-﻿Random rnd = new Random();
-int value = rnd.Next(0, 50);
-int number;
-int count = 0;
+﻿int start = 0;
+int stop = 20;
+
+Random rnd = new Random();
+int value = rnd.Next(start, stop);
+int number = 0;
+int count = 1;
 
 do
 {
-    Console.Write("Система загадала число, введите Ваш вариант : ");
-    while (!int.TryParse(Console.ReadLine(), out number))
-    {
-        Console.Write("Вы ввели не число, введите целое число : ");
-    }
+    Console.Write($"Угадайте число X (от {start} до {stop}) : ");
+    string input = Console.ReadLine();
+    while (input != "exit" && !int.TryParse(input , out number))
+        {
+            Console.Write("Ошибка ввода! Введите число : ");
+            input = Console.ReadLine();
+        }
+    if (input == "exit")
+        {
+            Console.WriteLine($"Выходим. Кол-во попыток: {count}");
+            break;
+        }
     Console.WriteLine($"Вы выбрали число {number}");
-    if (number == 999)
-    {
-        Console.WriteLine("Будет произведён выход ! Всего доброго!");
-        break;
+    switch (number) {
+        case ( > 0) when number == value: Console.WriteLine($"\nПоздравляем, Вы ответили верно c {count} попытки !"); break;
+        case ( > 0) when number < 0 || number > stop: Console.WriteLine($"Введёное число не входит в диапазон\n"); count++; break;
+        default: Console.WriteLine("Ответ неправильный, попробуйте снова!\n"); count++; break;
     }
-    if (value == number)
-    {
-        Console.WriteLine("Поздравляем, Вы ответили верно!");
-    }
-    else
-    {
-        Console.WriteLine("Ответ неправильный, попробуйте снова!");
-    }
-    Console.WriteLine("");
 } while (value != number);
